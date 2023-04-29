b9adad5ec6f0717ee90a8be42bb4941f6f5a60d6
sonsungjong <tjdwhd70599@gmail.com>
2023-04-30 06:20:05 +0900

    하위 디렉토리까지 스테이징

---
+++ basic/git.py
@@ -0,0 +1,16 @@
+'''
+git 설치
+
+git config --global user.email "깃허브이메일"
+git config --global user.name "깃허브이름"
+
+git init
+git add 파일명.확장자
+git commit -m "날짜 버전 메모"
+
+git status : 어떤 파일들을 스테이징 해놓았는지 확인
+git log --all --oneline : commit 을 조회
+
+'''
+# git add : 레포지토리에 저장할 파일을 스테이징함
+# git commit : 스테이징된 파일을 레포지토리에 저장
