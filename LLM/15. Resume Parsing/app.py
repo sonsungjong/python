dcc75f9ad700885c4753caed9d2f1aeb6871b0db
sonsungjong <tbxmtbfm@naver.com>
2025-03-20 12:19:08 +0900

    이력서 파싱싱

--- LLM/15. Resume Parsing/app.py
+++ LLM/15. Resume Parsing/app.py
@@ -1,6 +1,4 @@
-# Streamlit Application Video and Playlist
-# https://www.youtube.com/watch?v=hff2tHUzxJM&list=PLc2rvfiptPSSpZ99EnJbH5LjTJ_nOoSWW
-
+# streamlit run app.py
 import streamlit as st
 import pymupdf
 
@@ -10,6 +8,7 @@ st.title("Resume Parsing")
 st.write("Upload a resume in PDF format to extract information")
 
 uploaded_file = st.file_uploader("Choose a file")
+context = ""
 
 if uploaded_file is not None:
     bytearray = uploaded_file.read()
@@ -21,20 +20,23 @@ if uploaded_file is not None:
 
     pdf.close()
 
+    # st.write(context)
 
-question = """You are tasked with parsing a job resume. Your goal is to extract relevant information in a valid structured 'JSON' format. 
-                Do not write preambles or explanations."""
+# 질문 프롬프트를 작성한다
+question = """
+You are tasked with parsing a job resume. 
+Your goal is to extract relevant information in a valid structured 'JSON' format.
+Do not write preambles or explanations.
+"""
 
-if st.button("Parse Resume"):
+if st.button("Pagese PDF"):
     with st.spinner("Parsing Resume..."):
         response = ask_llm(context=context, question=question)
 
-    
-    with st.spinner("Validating JSON..."):
+    with st.spinner("Vaildating JSON..."):
         response = validate_json(response)
 
-    
-    st.write("**Extracted Information**")
+    st.write("**Extracted Information")
     st.write(response)
 
     st.write("You can copy the JSON output and use it in your application.")
