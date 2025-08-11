33960ca1dc6f8c33dcdd69a6ec8c2c463237654c
sonsungjong <tbxmtbfm@naver.com>
2025-08-11 13:01:41 +0900

    dasda

---
+++ RAG/test.py
@@ -0,0 +1,13 @@
+# https://wikidocs.net/book/14314                           # 위키독스 주소
+# https://github.com/teddylee777/langchain-kr               # 실습코드 주소
+
+# 파워쉘 관리자권한 실행 후
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+# git clone https://github.com/pyenv-win/pyenv-win.git "$env:USERPROFILE\.pyenv"
+# [System.Environment]::SetEnvironmentVariable('PYENV', $env:USERPROFILE + "\.pyenv\pyenv-win\", "User")
+# [System.Environment]::SetEnvironmentVariable('PYENV_ROOT', $env:USERPROFILE + "\.pyenv\pyenv-win\", "User")
+# [System.Environment]::SetEnvironmentVariable('PYENV_HOME', $env:USERPROFILE + "\.pyenv\pyenv-win\", "User")
+
+# [System.Environment]::SetEnvironmentVariable('PATH', $env:USERPROFILE + "\.pyenv\pyenv-win\bin;" + $env:USERPROFILE + "\.pyenv\pyenv-win\shims;" + [System.Environment]::GetEnvironmentVariable('PATH', "User"), "User")
+
+# pyenv
