190db09940be63ea1ec2f563750838e86f678fe6
sonsungjong <tbxmtbfm@naver.com>
2025-09-21 20:20:56 +0900

    프롬프트 변경 챗봇

--- RAG/03-Chatbot/main.py
+++ RAG/03-Chatbot/main.py
@@ -7,6 +7,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
+from langchain import hub
+# from langchain_teddynote import load_prompt
+import yaml
 
 load_dotenv()
 
@@ -20,6 +23,7 @@ if "messages" not in st.session_state:
 # 사이드바 생성
 with st.sidebar:
     clear_btn = st.button("대화 초기화")        # 대화기록 초기화
+    selected_prompt = st.selectbox("프롬프트를 선택해 주세요", ("기본모드", "SNS 게시글", "요약"), index=0)
 
 # 기록된 대화 출력
 def print_messages():
@@ -30,14 +34,28 @@ def print_messages():
 def add_message(role, message):
     st.session_state["messages"].append(ChatMessage(role=role, content=message))
 
-def create_chain():
+def create_chain(prompt_type):
     prompt = ChatPromptTemplate.from_messages(
         [
             ("system", "당신은 친절한 AI 어시스턴트 입니다."),
             ("user", "#Question:\n{question}")
         ]
     )
-    llm = ChatOllama(model="llama3.2:1b", temperature=0)
+    if prompt_type == "SNS 게시글":
+        # 프롬프트 파일에서 불러오기
+        with open("prompts/sns.yaml", "r", encoding="utf-8") as f:
+            sns_prompt_data = yaml.safe_load(f)
+        prompt = ChatPromptTemplate.from_template(sns_prompt_data["template"])
+    elif prompt_type == "요약":
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", "당신은 친절한 AI 요약 어시스턴트입니다. 사용자가 입력한 문장을을 간결하게 요약해 주세요."),
+                ("user", "#요약할 내용:\n{question}")
+            ]
+        )
+    
+    # llm = ChatOllama(model="llama3.2:1b", temperature=0)
+    llm = ChatOllama(model="gpt-oss:20b", temperature=0)
     chain = prompt | llm | StrOutputParser()
     return chain
 
@@ -52,7 +70,7 @@ if user_input:
     st.chat_message("user").write(user_input)
     add_message("user", user_input)
     # 체인 생성
-    chain = create_chain()
+    chain = create_chain(selected_prompt)
     # 답변
     # ai_answer = chain.invoke({"question": user_input})
     ai_answer = ""
