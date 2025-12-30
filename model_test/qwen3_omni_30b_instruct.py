6677a59470757e723af8e0f3f86e016333c0a6cf
sonsungjong <tbxmtbfm@naver.com>
2025-12-30 18:01:30 +0900

    omni 테스트 중

---
+++ model_test/qwen3_omni_30b_instruct.py
@@ -0,0 +1,71 @@
+# pip install -U "huggingface_hub[cli]"
+# pip install bitsandbytes accelerate
+# huggingface-cli download Qwen/Qwen3-Omni-30B-A3B-Instruct --local-dir ./Qwen3-Omni-30B-A3B-Instruct
+# pip install qwen-omni-utils -U
+
+import soundfile as sf
+from transformers import Qwen3OmniMoeForConditionalGeneration, Qwen3OmniMoeProcessor, BitsAndBytesConfig
+from qwen_omni_utils import process_mm_info
+
+MODEL_PATH = "Qwen/Qwen3-Omni-30B-A3B-Instruct"
+
+# 4bit 양자화 설정
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,                      # 4bit 양자화 활성화
+    bnb_4bit_quant_type="nf4",              # NormalFloat4 (추천)
+    bnb_4bit_compute_dtype="bfloat16",      # 연산 시 bf16 사용
+    bnb_4bit_use_double_quant=True,         # 이중 양자화 (메모리 추가 절약)
+)
+
+model = Qwen3OmniMoeForConditionalGeneration.from_pretrained(
+    MODEL_PATH,
+    # dtype="auto",
+    quantization_config=bnb_config,         # 4bit 양자화 적용
+    device_map="auto",
+    # attn_implementation="flash_attention_2",    # ARM64는 안되는 듯?
+)
+
+processor = Qwen3OmniMoeProcessor.from_pretrained(MODEL_PATH)
+
+conversation = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen3-Omni/demo/cars.jpg"},
+            {"type": "audio", "audio": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen3-Omni/demo/cough.wav"},
+            {"type": "text", "text": "What can you see and hear? Answer in one short sentence in Korean."}
+        ],
+    },
+]
+
+# Set whether to use audio in video
+USE_AUDIO_IN_VIDEO = True
+
+# Preparation for inference
+text = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
+audios, images, videos = process_mm_info(conversation, use_audio_in_video=USE_AUDIO_IN_VIDEO)
+inputs = processor(text=text, 
+                   audio=audios, 
+                   images=images, 
+                   videos=videos, 
+                   return_tensors="pt", 
+                   padding=True, 
+                   use_audio_in_video=USE_AUDIO_IN_VIDEO)
+inputs = inputs.to(model.device).to(model.dtype)
+
+# Inference: Generation of the output text and audio
+text_ids, audio = model.generate(**inputs, 
+                                 speaker="Chelsie", 
+                                 thinker_return_dict_in_generate=True,
+                                 use_audio_in_video=USE_AUDIO_IN_VIDEO)
+
+text = processor.batch_decode(text_ids.sequences[:, inputs["input_ids"].shape[1] :],
+                              skip_special_tokens=True,
+                              clean_up_tokenization_spaces=False)
+print(text)
+if audio is not None:
+    sf.write(
+        "qwen3_omni.wav",
+        audio.reshape(-1).detach().cpu().numpy(),
+        samplerate=24000,
+    )
